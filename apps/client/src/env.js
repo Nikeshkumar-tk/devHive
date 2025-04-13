76e05fb34d4ae6d48f70254eabda3925b88ada29
Nikeshkumar-tk <nikeshkumartk2020@gmail.com>
2025-04-13 12:48:51 +0530

    adding node env to oss

--- apps/client/src/env.js
+++ apps/client/src/env.js
@@ -6,17 +6,17 @@ export const env = createEnv({
    * Specify your server-side environment variables schema here. This way you can ensure the app
    * isn't built with invalid env vars.
    */
-  // server: {
-  //   AUTH_SECRET:
-  //     process.env.NODE_ENV === "production"
-  //       ? z.string()
-  //       : z.string().optional(),
-  //   AUTH_DISCORD_ID: z.string(),
-  //   AUTH_DISCORD_SECRET: z.string(),
-  //   NODE_ENV: z
-  //     .enum(["development", "test", "production"])
-  //     .default("development"),
-  // },
+  server: {
+    // AUTH_SECRET:
+    //   process.env.NODE_ENV === "production"
+    //     ? z.string()
+    //     : z.string().optional(),
+    // AUTH_DISCORD_ID: z.string(),
+    // AUTH_DISCORD_SECRET: z.string(),
+    NODE_ENV: z
+      .enum(["development", "test", "production"])
+      .default("development"),
+  },
 
   /**
    * Specify your client-side environment variables schema here. This way you can ensure the app
@@ -32,9 +32,9 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    AUTH_SECRET: process.env.AUTH_SECRET,
-    AUTH_DISCORD_ID: process.env.AUTH_DISCORD_ID,
-    AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET,
+    // AUTH_SECRET: process.env.AUTH_SECRET,
+    // AUTH_DISCORD_ID: process.env.AUTH_DISCORD_ID,
+    // AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET,
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
