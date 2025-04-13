60a39163e1b7f8bf0210f868355b6d3645362bf8
Nikeshkumar-tk <nikeshkumartk2020@gmail.com>
2025-04-13 12:45:14 +0530

    removing the env from oss

--- apps/client/src/env.js
+++ apps/client/src/env.js
@@ -6,26 +6,26 @@ export const env = createEnv({
    * Specify your server-side environment variables schema here. This way you can ensure the app
    * isn't built with invalid env vars.
    */
-  server: {
-    AUTH_SECRET:
-      process.env.NODE_ENV === "production"
-        ? z.string()
-        : z.string().optional(),
-    AUTH_DISCORD_ID: z.string(),
-    AUTH_DISCORD_SECRET: z.string(),
-    NODE_ENV: z
-      .enum(["development", "test", "production"])
-      .default("development"),
-  },
+  // server: {
+  //   AUTH_SECRET:
+  //     process.env.NODE_ENV === "production"
+  //       ? z.string()
+  //       : z.string().optional(),
+  //   AUTH_DISCORD_ID: z.string(),
+  //   AUTH_DISCORD_SECRET: z.string(),
+  //   NODE_ENV: z
+  //     .enum(["development", "test", "production"])
+  //     .default("development"),
+  // },
 
   /**
    * Specify your client-side environment variables schema here. This way you can ensure the app
    * isn't built with invalid env vars. To expose them to the client, prefix them with
    * `NEXT_PUBLIC_`.
    */
-  client: {
-    // NEXT_PUBLIC_CLIENTVAR: z.string(),
-  },
+  // client: {
+  //   // NEXT_PUBLIC_CLIENTVAR: z.string(),
+  // },
 
   /**
    * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
