7907220a3ce044360021fb5ca5fb8f04cecff711
Nikeshkumar-tk <nikeshkumartk2020@gmail.com>
2025-04-13 12:54:27 +0530

    fixing lint issues

--- apps/client/sst.config.ts
+++ apps/client/sst.config.ts
@@ -2,16 +2,17 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 //@ts-ignore
 export default $config({
-    app(input) {
-        return {
-            name: 'web',
-            removal: input?.stage === 'production' ? 'retain' : 'remove',
-            protect: ['production'].includes(input?.stage),
-            home: 'aws',
-        };
-    },
-    async run() {
-        //@ts-ignore
-        new sst.aws.Nextjs('MyWeb');
-    },
+  //@ts-ignore
+  app(input) {
+    return {
+      name: "web",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      protect: ["production"].includes(input?.stage),
+      home: "aws",
+    };
+  },
+  async run() {
+    //@ts-ignore
+    new sst.aws.Nextjs("MyWeb");
+  },
 });
